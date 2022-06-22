55389d77d54ed79d0be0a39fb9a073c47b7763b4
Maikl <raids@yandex.ru>
2022-06-22 11:55:24 +0300

    6

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 from json.decoder import JSONDecodeError
 import requests
-
-payload = {"login": "secret_login",
-           "password": "secret_pass"}
+import pytest
 
 
-response = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
-
-print(response.status_code)
-print(response.text)
-print(dict(response.cookies))
-cookie_value = response.cookies
-
-response2 = requests.get("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookie_value)
-print(response2.text)
 # try:
 #     parsed_response_text = response.json()
 #     print(parsed_response_text)
