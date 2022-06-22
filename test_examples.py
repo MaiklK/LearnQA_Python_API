68cf6bdb6a818b9faebc089b67187311b77a1b9a
Maikl <raids@yandex.ru>
2022-06-22 12:41:50 +0300

    7

---
+++ test_examples.py
@@ -0,0 +1,12 @@
+class TestExample:
+    def test_check_math(selfs):
+        a = 5
+        b = 9
+        expected_sum = 14
+        assert a + b == expected_sum, f"Sum of variables a and b is not equal to {expected_sum}"
+
+    def test_check_math1(selfs):
+        a = 5
+        b = 6
+        expected_sum = 14
+        assert a + b == expected_sum, f"Sum of variables a and b is not equal to {expected_sum}"
