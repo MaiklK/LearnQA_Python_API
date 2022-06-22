973e09b1f480703646277e2fc5ac884134ba617f
Maikl <raids@yandex.ru>
2022-06-22 11:41:06 +0300

    4

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from json.decoder import JSONDecodeError
 import requests
 
-payload = {"name": "Raids"}
-key = 'answer'
+payload = {"login": "secret_login",
+           "password": "secret_pass"}
 
-response = requests.post("https://playground.learnqa.ru/api/check_type", data=payload)
+
+response = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
+
+print(response.status_code)
 print(response.text)
-try:
-    parsed_response_text = response.json()
-    print(parsed_response_text)
-except JSONDecodeError:
-    print("Response in not a JSON format")
+print(dict(response.cookies))
+cookie_value = response.cookies
+
+response2 = requests.get("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookie_value)
+print(response2.text)
+# try:
+#     parsed_response_text = response.json()
+#     print(parsed_response_text)
+# except JSONDecodeError:
+#     print("Response in not a JSON format")
